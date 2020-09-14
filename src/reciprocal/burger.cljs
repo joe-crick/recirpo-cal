f0b5507c1b37d1e9f11c5738a6508891bf6aea67
Joe Crick <joseph.crick@finreach.com>
2020-09-14 15:28:17 +0200

    Basic transitions for burger menu. Add icons.

--- src/reciprocal/burger.cljs
+++ src/reciprocal/burger.cljs
@@ -5,21 +5,38 @@
     [stylefy.core :as stylefy
      :refer           [use-style]]))
 
-(def show-style
+(def show-layout
   {:position   "absolute"
    :left       "10px"
-   :transition "all 1s ease-out"})
+   :transition "all 1s ease-in-out"})
 
-(def hide-style {:position "absolute" :left "-500px"})
+(def hide-layout
+  (merge show-layout {:left "-700px"}))
+
+(def menu-style
+  {:width            "33%"
+   :background-color "#efefe3"})
+
+(def icon-style
+  {:width "40px"})
+
+(def button-style
+  {:background "none"
+   :border     "0"
+   :cursor     "pointer"
+   :outline    "0"})
 
 (defn burger [children]
   (let [is-open (atom false)]
     (fn [children]
       [:div
        [:button
-        {:on-click (fn []
-                     (reset! is-open (not @is-open)))}
-        (if @is-open "Close" "Open")]
+        (merge (use-style button-style)
+               {:on-click (fn []
+                            (reset! is-open (not @is-open)))})
+        [:img
+         (merge (use-style icon-style)
+                {:src (if @is-open "images/burger-close.svg" "images/burger-menu.svg")})]]
        [:div
-        (use-style (if @is-open show-style hide-style))
+        (use-style (merge (if @is-open show-layout hide-layout) menu-style))
         [children]]])))
